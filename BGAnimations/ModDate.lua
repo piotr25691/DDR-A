7dba708e15f4066ef69a7550bbbdbae81b7fbe2d
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2023-01-02 10:22:53 +0100

    update build date to final

--- BGAnimations/ModDate.lua
+++ BGAnimations/ModDate.lua
@@ -2,7 +2,7 @@ return Def.BitmapText{
       Font="_futura pt medium 30px";
       InitCommand=function(s)  
 	  --         MDX:A:A:A:Year/Month/Day/00
-	  s:settext("MDX:U:J:A:2019042200")
+	  s:settext("MDX:J:A:A:2019072400")
 	  s:halign(0)
 	  s:xy(SCREEN_LEFT+8.5,SCREEN_TOP+13)
 	  s:zoomx(0.62)
